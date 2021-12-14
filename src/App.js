f5541c2aeeae16ca386ce848a849dd3a4eca3cc5
Sergio Figueroa <figueroa.sergio.26@gmail.com>
2021-12-14 17:15:22 +0100

    custom hook

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import { TodoSearch } from "./componets/TodoSearch";
 import { TodoItem } from "./componets/TodoItem";
 import { TodoList } from "./componets/TodoList";
 import { CreateTodoButton } from "./componets/CreateTodoButton";
-let defaultTodos = [
-  { text: "cortar cebolla", completed: false },
-  { text: "tallar cebolla 2", completed: true },
-  { text: "cortar cebolla 3", completed: false },
-];
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = useState("");
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
   const totalTodos = todos.length;
@@ -27,17 +43,18 @@ function App() {
       return text.includes(searcText);
     });
   }
+
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
   return (
     <>
